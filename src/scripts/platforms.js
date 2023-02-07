e55673295e8c40349aa0997515efe6e166b23e3a
Nestor Vega <vega.nestor1@gmail.com>
2023-02-06 16:55:59 -0800

    img await

--- src/scripts/platforms.js
+++ src/scripts/platforms.js
@@ -4,14 +4,15 @@ class Platform{
         this.plateformImage = new Image()
         this.plateformImage.onload = () => {
             // const img = await loadImage('../images/background.png')
+            this.ctx.drawImage(this.plateformImage, this.position.x, this.position.y, this.width, this.height)
             console.log(this.plateformImage)
-            this.position = {
-                x,
-                y
-            }
-            this.width = 250
-            this.height = 150
         }
+        this.position = {
+            x,
+            y
+        }
+        this.width = 250
+        this.height = 150
         this.plateformImage.src = '../images/background.png'
     }
     draw(){
