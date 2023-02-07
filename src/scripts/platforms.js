c4ba766a445783f0a7d36d19f9f8f9592ec99d5f
Nestor Vega <vega.nestor1@gmail.com>
2023-02-06 16:47:37 -0800

    move img src

--- src/scripts/platforms.js
+++ src/scripts/platforms.js
@@ -2,7 +2,6 @@ class Platform{
     constructor({x, y}, ctx){
         this.ctx = ctx
         this.plateformImage = new Image()
-        this.plateformImage.src = '../images/background.png'
         this.plateformImage.onload = () => {
             // const img = await loadImage('../images/background.png')
             console.log(this.plateformImage)
@@ -13,6 +12,7 @@ class Platform{
             this.width = 250
             this.height = 150
         }
+        this.plateformImage.src = '../images/background.png'
     }
     draw(){
         this.ctx.drawImage(this.plateformImage, this.position.x, this.position.y, this.width, this.height)
